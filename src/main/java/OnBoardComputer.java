e9471dcfdf271e2e8166ae59b0b592eaf63af70b
Ricky Kyte <137026079+rpk2301@users.noreply.github.com>
2023-07-30 18:23:17 -0400

    Test To Windows

--- src/main/java/OnBoardComputer.java
+++ src/main/java/OnBoardComputer.java
@@ -1,11 +1,17 @@
 public class OnBoardComputer implements BurnStream {
 
-    @Override
-    public int getNextBurn(DescentEvent status) {
-        int burn = 0;
-        
-        System.out.println(burn); /*hack!*/
-        return burn;
+         @Override
+        public int getNextBurn(DescentEvent status) {
+
+             int Altitude = status.Altitude;
+             int Velocity = status.Velocity;
+             double HowManyToGround = Altitude/Velocity;
+             while(Altitude>1000) {
+                 if (HowManyToGround >= 10 && Altitude > 1000) {
+                     return 0;
+                 } else return 200;
+             }
+             return 45;
     }
 
 }
